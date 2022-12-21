f1bdbc9798bdcdace86f33be3e9c4e091bdaff77
99JeonJongMin <jnjjb05@naver.com>
2022-12-21 23:03:07 +0900

    1st commit

---
+++ 기말과제.js
@@ -0,0 +1,158 @@
+//////////////////////////////
+//전역변수
+let dragging =null; //드래깅 중인 카드 객체 등록
+let dragCard = null;
+let dragOver = null; // 드래깅 객체가 올라간 객체 등록
+// let quizWords = quizSet.quiz.split(" ");
+
+
+/////////////////////////
+//과제 관련
+function random (num){
+    return Math.floor(Math.random() * num)
+}
+//words 배열에 있는 단어들로 카드들을 생성하는 HTML 문장을 완성하는 함수
+function getCardsWords(words,kor,celebrity) {
+    let cardsHTML ="";
+    let aCardHTML ="";
+    let count=1;
+    for (let word of words) {
+        aCardHTML = `<div draggable = "true" originalOrder="${count++}"class="card">${word}</div>`;
+        
+        cardsHTML += aCardHTML;
+    }
+    // box 안에 위에서 생성한 카드들을 추가한다.
+    document.querySelector(".box").innerHTML = cardsHTML;
+    document.querySelector(".kor").innerHTML = kor;
+    document.querySelector(".celebrity").innerHTML = celebrity;
+
+}
+
+function compareAnswer() {
+    cardContents = document.querySelectorAll(".card");
+    let myAnswer = "";
+    for (let item of cardContents) {
+      myAnswer += item.innerText;
+    }
+    console.log(myAnswer);
+  
+    let answerContents = quizSet.quiz[quizNo].eng.split(" ");
+    let answer = "";
+    console.log("eng " + answerContents);
+  
+    for (let item of answerContents) {
+      answer += item;
+    }
+    console.log(answer);
+  
+    if (myAnswer == answer) {
+      alert("정답입니다.");
+      location.reload();
+      if (localStorage.counter == undefined) {
+        localStorage.setItem("counter", 0);
+      }
+      localStorage.counter++;
+    } else {
+      alert("다시 생각해보세요.");
+    }
+  }
+
+function selectQuiz(quizNo){
+    if(quizNo >= quizSet.quiz.length)
+        console.log("에러 1: 퀴즈 번호가 범위를 벗어났습니다.");
+    let words = quizSet.quiz[quizNo].eng.split(" ");
+    let kor = quizSet.quiz[quizNo].kor;
+    let celebrity =quizSet.quiz[quizNo].celebrity;
+    getCardsWords(words,kor,celebrity);
+}
+
+function qsa(el) {
+    return document.querySelectorAll(el);
+}
+
+
+//////////////////
+//class card 객체 이벤트 핸들러 영역
+function dragStartCard (ev) {
+    dragging = this;
+    this.classList.add("dragging");
+}
+
+function dragEndCard(ev) {
+    ev.preventDefault(); // 브라우저의 default값을 미연에 차단 (방해를 받을 수 있음)
+    this.classList.remove("dragging");
+    dragging = null;
+    //dragOver가 null이 아니면 처리할 내용
+    if(dragOver) {// != null 이 아니라도 null값이 아닌값이 들어가면 참으로 들어감
+        dragOver.classList.remove("dragOver");
+        dragOver = null;
+    }
+    //dragCard가 null이 아니면 처리할 내용
+    if(dragCard) {// != null 이 아니라도 null값이 아닌값이 들어가면 참으로 들어감
+        dragCard.classList.remove("overCard");
+        dragCard = null;
+    }
+}
+
+/////////////////
+//class card 객체 이벤트 핸들러 영역
+function dragOverCard(ev) {
+    ev.preventDefault();
+    ev.stopPropagation(); // dragOverCard 이벤트가 부모 노드로 전파
+                            // 되는것을 차단
+    dragCard = this;
+    this.classList.add("overCard");
+
+}
+
+function dragLeaveCard(ev) {
+    ev.preventDefault();
+    dragCard = null;
+    this.classList.remove("overCard");
+}
+
+function dropCard(ev) {
+    this.parentNode.insertBefore(dragging,this);
+}
+
+
+///////////////////////////
+//class box 객체 이벤트 핸들러 영역
+function dragOverBox(ev) {
+    ev.preventDefault();
+    dragOver = this;
+    this.classList.add("dragOver");
+} 
+
+function dragLeaveBox(ev){
+    ev.preventDefault();
+    dragOver = null;
+    this.classList.remove("dragOver");
+}
+
+function dropBox(ev) {
+    dragOver.appendChild(dragging);
+}
+
+////////////////
+
+window.onload = function() {
+    // 단어 card 들 먼저 생성
+    selectQuiz(random(5));
+    // card 객체 이벤트 핸들러 연결
+    let cardArray = qsa(".card");
+    for(let card of cardArray){
+        card.addEventListener("dragstart", dragStartCard);
+        card.addEventListener("dragend",dragEndCard);
+        card.addEventListener("dragover",dragOverCard);
+        card.addEventListener("dragleave",dragLeaveCard);
+        card.addEventListener("drop",dropCard);
+    }
+    let boxArray = qsa(".box");
+    for(let box of boxArray){
+        box.addEventListener("dragover", dragOverBox);
+        box.addEventListener("dragleave",dragLeaveBox);
+        box.addEventListener("drop",dropBox);
+
+    }
+}
---
+++ 기말퀴즈.js
@@ -0,0 +1,35 @@
+let quizSet ={
+    topic: "기말과제 예제 제시",
+    author: "전종민",
+    quiz: [
+        {
+            eng: "Boys, be ambitious",
+            kor: "소년이여, 야망을 가져라",
+            celebrity:"윌리엄 스미스 클라크"
+        },
+        {
+            eng: "Hold faithfulness and sincerity as first principles",
+            kor: "충심과 성실을 첫 번째 원칙으로 삼아라",
+            celebrity:"공자"
+        },
+        {
+            eng: "Don’t pay any attention to what they write about you. Just measure it in inches",
+            kor: "그들이 당신에 대해 뭐라고 쓰든 전혀 신경쓰지 마라. 다만 그것을 자세히 평가하라.",
+            celebrity:"앤디 워홀"
+        },
+        {
+            eng: "Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine",
+            kor: "오늘은 힘이 들고, 내일은 더 견디기 어려울 것입니다. 그러나 그 다음 날은 찬란할 겁니다",
+            celebrity:"마윈"
+        },
+        {
+            eng: "Choose rather to be strong of soul than strong of body",
+            kor: "강한 신체보다 강한 정신을 선택하라",
+            celebrity:"피타고라스"
+        },{
+            eng: "We don't make mistakes, we have happy accidents",
+            kor: "우리는 실수를 한 게 아닙니다. 단지 행복한 사고가 일어났 을 뿐이죠",
+            celebrity:"밥 로스"
+        }
+    ]
+}
