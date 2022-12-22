61c62fe7f20e508771c4fbf21c8ad20a972d809c
99JeonJongMin <jnjjb05@naver.com>
2022-12-22 17:29:41 +0900

    fin.commit

--- 기말과제.js
+++ 기말과제.js
@@ -3,6 +3,9 @@
 let dragging =null; //드래깅 중인 카드 객체 등록
 let dragCard = null;
 let dragOver = null; // 드래깅 객체가 올라간 객체 등록
+let count=0 ;
+let saveWords = [];
+let cardsHTML =[];
 // let quizWords = quizSet.quiz.split(" ");
 
 
@@ -11,26 +14,60 @@ let dragOver = null; // 드래깅 객체가 올라간 객체 등록
 function random (num){
     return Math.floor(Math.random() * num)
 }
+function suffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+function classcard(num){
+    return document.getElementsByClassName(`card${num}`);
+}
+
+function answer(drag,count) {
+    console.log(drag.id);
+    console.log(saveWords[1]);
+    // console.log(drag.innerText);
+}
+
+    
 //words 배열에 있는 단어들로 카드들을 생성하는 HTML 문장을 완성하는 함수
 function getCardsWords(words,kor,celebrity) {
-    let cardsHTML =[]
+   
+    
     let aCardHTML ="";
-    let count=0;
+    
+    for(let word of words){
+        saveWords.push(word);
+    }
+    console.log(saveWords);
     for (let word of words) {
-        aCardHTML = `<div draggable = "true" originalOrder="${count++}"class="card${count}">${word}</div>`;
+        aCardHTML = `<div draggable = "true" id="${count++}"class="card${count}">${word}</div>`;
         
         
         cardsHTML.push(aCardHTML);
-        
     }
+    
+    // console.log(count);
+    suffle(cardsHTML);
+    
+    
+    console.log(cardsHTML[0])
 
-    console.log(cardsHTML);
+
+    
+    saveWords = saveWords.join("","");
+    cardsHTML = cardsHTML.join("","");
+   
+    
     
     // box 안에 위에서 생성한 카드들을 추가한다.
-    document.querySelector(".box").innerHTML = cardsHTML.join("","");
+    document.querySelector(".ans").innerHTML = saveWords;
+    document.querySelector(".box").innerHTML = cardsHTML;
     document.querySelector(".kor").innerHTML = kor;
     document.querySelector(".celebrity").innerHTML = celebrity;
 
+
+    
+    
+    
 }
 
 
@@ -53,13 +90,17 @@ function qsa(el) {
 function dragStartCard (ev) {
     dragging = this;
     this.classList.add("dragging");
+    
 }
 
 function dragEndCard(ev) {
     ev.preventDefault(); // 브라우저의 default값을 미연에 차단 (방해를 받을 수 있음)
     this.classList.remove("dragging");
+    answer(dragging,count);
+    console.log(dragging.index);
     dragging = null;
     //dragOver가 null이 아니면 처리할 내용
+    
     if(dragOver) {// != null 이 아니라도 null값이 아닌값이 들어가면 참으로 들어감
         dragOver.classList.remove("dragOver");
         dragOver = null;
@@ -89,7 +130,9 @@ function dragLeaveCard(ev) {
 }
 
 function dropCard(ev) {
+    
     this.parentNode.insertBefore(dragging,this);
+    
 }
 
 
@@ -115,7 +158,7 @@ function dragLeaveBox(ev){
 
 window.onload = function() {
     // 단어 card 들 먼저 생성
-    selectQuiz(random(5));
+    selectQuiz(random(6));
     // card 객체 이벤트 핸들러 연결
     let cardArray = $("[class*='card']");
     for(let card of cardArray){
@@ -132,4 +175,5 @@ window.onload = function() {
         // box.addEventListener("drop",dropBox);
 
     }
+    
 }
--- 기말퀴즈.js
+++ 기말퀴즈.js
@@ -13,23 +13,29 @@ let quizSet ={
             celebrity:"공자"
         },
         {
-            eng: "Don’t pay any attention to what they write about you. Just measure it in inches",
+            eng: "Don't pay any attention to what they write about you. Just measure it in inches",
             kor: "그들이 당신에 대해 뭐라고 쓰든 전혀 신경쓰지 마라. 다만 그것을 자세히 평가하라.",
             celebrity:"앤디 워홀"
-        },
-        {
-            eng: "Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine",
-            kor: "오늘은 힘이 들고, 내일은 더 견디기 어려울 것입니다. 그러나 그 다음 날은 찬란할 겁니다",
-            celebrity:"마윈"
         },
         {
             eng: "Choose rather to be strong of soul than strong of body",
             kor: "강한 신체보다 강한 정신을 선택하라",
             celebrity:"피타고라스"
-        },{
+        },
+        {
+            eng: "You create your opportunities by asking for them.",
+            kor: "기회를 찾아야 기회를 만든다.",
+            celebrity: "패티 헨슨(Patty Hansen)"
+        },
+        {
             eng: "We don't make mistakes, we have happy accidents",
             kor: "우리는 실수를 한 게 아닙니다. 단지 행복한 사고가 일어났 을 뿐이죠",
             celebrity:"밥 로스"
+        },
+        {
+            eng: "Respect yourself and others will respect you.",
+            kor: "스스로를 존경하면 다른 사람도 당신을 존경할 것이다.",
+            celebrity: "공자 (Confucius)"
         }
     ]
 }
